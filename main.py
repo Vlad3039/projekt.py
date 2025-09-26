a5107324306b5a4265aa5f5537ef625dd54b3a77
Vlad3039 <130600836+Vlad3039@users.noreply.github.com>
2025-09-26 22:52:42 +0200

    test001

---
+++ main.py
@@ -0,0 +1,70 @@
+""" def alt(jahre):
+    print(f"bist du {jahre}")
+
+alt(18)
+alt(52)
+ """
+
+"""  import time
+
+print("На іншу любов, іншу любов")
+time.sleep(4)
+print("Сльози всі вже пролито")
+time.sleep(3)
+print("На іншу любов, іншу любов")
+time.sleep(2)
+print("Сльози всі вже пролито, о-о")
+ """ 
+
+""" import random
+namber = random.randint(1, 43)
+print("zeil ist", namber) """
+
+
+""" import time
+
+print("Мы выросли там где нет нормы морали")
+time.sleep(0.3)
+print("По спирали вниз, над нами стая птиц кружит")
+time.sleep(3)
+print("И я прошу не вспоминай меня пока я здесь, пока я еще жив")
+ """
+
+""" 
+import time
+import random
+namber = random.randint(1, 19)
+def untericht(lernen):
+    print(f"в мене {lernen}",)
+
+untericht(namber)
+time.sleep(3)
+untericht("матиматика")  """
+
+
+
+
+""" # Введення (отримання даних)
+vlados = input("Введіть ваше ім'я: ")
+
+# Перетворення (обробка даних)
+hallo = (f"Привіт, {vlados}!")
+
+# Виведення (виведення даних)
+print(hallo)
+ """
+
+""" begrüsung = input("Введіть ваше ім'я: ")
+
+hallo = (f"Hallo schon mit dir kennen zu lehrnen {begrüsung}")
+
+print(hallo) """
+
+
+""" import time
+
+namber = input("Write down the number: ")
+
+namber_teip = (f"Thank you for the number {namber}")
+
+print(namber_teip) """
